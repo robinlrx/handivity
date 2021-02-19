1ce35ccaa84e0b6c9c83362232a4eea6588d7331
robinlrx <robin@monkey-lab.io>
2021-02-19 14:18:24 +0100

    change url site and connexion

--- php/connexion.php
+++ php/connexion.php
@@ -1,3 +1,3 @@
 <?php 
-	$db=new PDO('mysql:host=db5001464205.hosting-data.io;dbname=dbs1232141;port=3306;charset=utf8','dbu340265','FarahCamila11!');
+	$db=new PDO('mysql:host=host;dbname=dbname;port=3306;charset=utf8','user','password');
 ?>
--- presentation_parasportif.php
+++ presentation_parasportif.php
@@ -208,7 +208,7 @@ l-964 0 -79 -107 c-44 -60 -190 -256 -325 -438 -135 -181 -286 -384 -335 -450
 				</div><img alt="" src="./images/cartes/<?php echo $result["carte"] ?>.png">
 				<div class="bas">
 					<div class="lien"><input id="to-copy" aria-label="URL de la page"
-							value="https://www.handivity.robinleroux.fr/presentation_parasportif.php?id=<?php echo $result["id_sportif"] ?>"
+							value="url-site/presentation_parasportif.php?id=<?php echo $result["id_sportif"] ?>"
 							name="lien" readonly>
 						<div class="js-copy" id="copy" alt="copier"><svg height="15pt" viewBox="-40 0 512 512"
 								width="15pt" fill="#209EBB">
@@ -217,13 +217,13 @@ l-964 0 -79 -107 c-44 -60 -190 -256 -325 -438 -135 -181 -286 -384 -335 -450
 								</svg></div>
 					</div>
 					<div class="reseaux"><button class="button share_facebook"
-							data-url="https://handivity.robinleroux.fr/presentation_parasportif.php?id=<?php echo $result["id_sportif"] ?>"
+							data-url="url-site/presentation_parasportif.php?id=<?php echo $result["id_sportif"] ?>"
 							alt="Partager sur facebook" title="Partager sur facebook"><svg height="40pt"
 								viewBox="0 0 512 512" width="40pt" fill="#FFFFFF">
 								<path
 									d="m475.074219 0h-438.148438c-20.394531 0-36.925781 16.53125-36.925781 36.925781v438.148438c0 20.394531 16.53125 36.925781 36.925781 36.925781h236.574219v-198h-66.5v-77.5h66.5v-57.035156c0-66.140625 40.378906-102.140625 99.378906-102.140625 28.257813 0 52.542969 2.105469 59.621094 3.046875v69.128906h-40.683594c-32.101562 0-38.316406 15.253906-38.316406 37.640625v49.359375h76.75l-10 77.5h-66.75v198h121.574219c20.394531 0 36.925781-16.53125 36.925781-36.925781v-438.148438c0-20.394531-16.53125-36.925781-36.925781-36.925781zm0 0" />
 								</svg></button> <button class="button share_twitter"
-							data-url="https://handivity.robinleroux.fr/presentation_parasportif.php?id=<?php echo $result["id_sportif"] ?>"
+							data-url="url-site/presentation_parasportif.php?id=<?php echo $result["id_sportif"] ?>"
 							alt="Partager sur twitter" title="Partager sur twitter"><svg height="40pt"
 								viewBox="0 0 512 512" width="40pt" fill="#FFFFFF">
 								<path
